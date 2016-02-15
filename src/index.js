e12075842038533c07312e7847679fa7ffca17d9
Jannis R <mail@jannisr.de>
2016-02-15 20:12:27 +0100

    more readable code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const path =    require('path')
-const home =    require('os-homedir')()
+const homedir = require('os-homedir')
 const Promise = require('bluebird')
 const fs =      require('fs-promise')
 
@@ -9,79 +9,85 @@ const fs =      require('fs-promise')
 
 
 
-module.exports = (file) => {
-	if (!file) file = path.join(home, 'time-tracking/trackers.json')
-	let dir = path.dirname(file)
-	let self = {
+const Track = Object.freeze({
+
+	file: path.join(homedir(), 'time-tracking/trackers.json'),
+
+	read: Promise.coroutine(function* (name) {
+		let dir = path.dirname(this.file)
+		let stats = yield fs.stat(dir)
+		if (!stats || !stats.isDirectory()) throw new Error(`${dir} is not a directory`)
+		let trackers = JSON.parse(yield fs.readFile(this.file))
+		if (!name) return trackers
+		if (trackers[name]) return trackers[name]
+		throw new Error(`${name} doesn't exist.`)
+	}),
+
+	_write: function (trackers) {
+		return fs.writeFile(this.file, JSON.stringify(trackers))
+	},
 
 
 
-		read: Promise.coroutine(function* (name) {
-			let stats = yield fs.stat(dir)
-			if (!stats || !stats.isDirectory()) throw new Error(`${dir} is not a directory`)
-			let trackers = JSON.parse(yield fs.readFile(file))
-			if (!name) return trackers
-			if (trackers[name]) return trackers[name]
-			throw new Error(`${name} doesn't exist.`)
-		}),
-
-
-
-		_write: ((trackers) => fs.writeFile(file, JSON.stringify(trackers))),
-
-
-
-		start: Promise.coroutine(function* (name) {
-			let now = Date.now()
-			let result = {}
-			let trackers = yield self.read()
-			if (!trackers[name]) {
-				result.isNew = true
+	start: Promise.coroutine(function* (name) {
+		let now = Date.now()
+		let result = {}
+		let trackers = yield this.read()
+		if (!trackers[name]) {
+			result.isNew = true
+			result.wasRunning = false
+			trackers[name] = {name: name, started: now, value: 0}
+		} else {
+			result.isNew = false
+			if (trackers[name].started) result.wasRunning = true
+			else {
 				result.wasRunning = false
-				trackers[name] = {name: name, started: now, value: 0}
-			} else {
-				result.isNew = false
-				if (trackers[name].started) result.wasRunning = true
-				else {
-					result.wasRunning = false
-					trackers[name].started = now
-				}
+				trackers[name].started = now
 			}
-			yield self._write(trackers)
-			return result
-		}),
+		}
+		yield this._write(trackers)
+		return result
+	}),
+
+	stop: Promise.coroutine(function* (name) {
+		let now = Date.now()
+		let result = {}
+		let trackers = yield this.read()
+		if (!trackers[name]) throw new Error(`${name} doesn't exist.`)
+		if(trackers[name].started) {
+			result.wasRunning = true
+			trackers[name].value += now - trackers[name].started
+			trackers[name].started = false
+		} else result.wasRunning = false
+		yield this._write(trackers)
+		return result
+	}),
 
 
 
-		stop: Promise.coroutine(function* (name) {
-			let now = Date.now()
-			let result = {}
-			let trackers = yield self.read()
-			if (!trackers[name]) throw new Error(`${name} doesn't exist.`)
-			if(trackers[name].started) {
-				result.wasRunning = true
-				trackers[name].value += now - trackers[name].started
-				trackers[name].started = false
-			} else result.wasRunning = false
-			yield self._write(trackers)
-			return result
-		}),
+	add: Promise.coroutine(function* (name, amount) {
+		debugger
+		let now = Date.now()
+		let trackers = yield this.read()
+		if (!trackers[name]) throw new Error(`${name} doesn't exist.`)
+		trackers[name].value += amount
+		console.log('trackers', trackers)
+		yield this._write(trackers)
+		return null
+	})
+
+})
 
 
 
-		add: Promise.coroutine(function* (name, amount) {
-			debugger
-			let now = Date.now()
-			let trackers = yield self.read()
-			if (!trackers[name]) throw new Error(`${name} doesn't exist.`)
-			trackers[name].value += amount
-			console.log('trackers', trackers)
-			yield self._write(trackers)
-			return null
-		})
-
-
-
-	}
-	return self
+const create = (file) => {
+	// https://gist.github.com/derhuerst/a585c4916b1c361cc6f0
+	let track = Object.create(Track)
+	if ('string' === typeof file) track.file = file
+	return track
 }
+create.Track = Track
+
+
+
+module.exports = create
