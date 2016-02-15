dbadf3870448698fa777af9b2f74fcfc483100c1
Jannis R <mail@jannisr.de>
2016-02-15 19:55:55 +0100

    rewrite in plain JS

--- bin/track.js
+++ bin/track.js
@@ -1,168 +1,216 @@
-#!env node
-// time-tracking 0.2.0
-var add, argv, chalk, figures, help, lPad, ms, options, rPad, showError, singleStatus, start, status, stop, subtract, track, yargs;
+#!/usr/bin/env node
+'use strict'
 
-yargs = require('yargs');
+const chalk =   require('chalk')
+const lPad =    require('left-pad')
+const rPad =    require('right-pad')
+const ms =      require('ms')
+const yargs =   require('yargs')
+const Promise = require('bluebird')
 
-chalk = require('chalk');
+const track =   require('../src/index')()
 
-lPad = require('left-pad');
 
-rPad = require('right-pad');
 
-ms = require('ms');
 
-track = require('../src/index');
 
-figures = {
-  started: chalk.green('\u25b6'),
-  stopped: chalk.red('\u25a0'),
-  error: chalk.red('!')
-};
-
-showError = function(err) {
-  return process.stderr.write([figures.error, err.message].join(' ') + '\n');
-};
-
-help = [chalk.yellow('track start <name>'), chalk.yellow('track - <name>'), '  Start a new or resume an existing tracker. `name` must be a valid JSON key.', chalk.yellow('track stop <name>'), chalk.yellow('track . <name>'), '  Stop an existing tracker.', '', chalk.yellow('track add <name> <amount>'), chalk.yellow('track + <name> <amount>'), '  Add any amount of time to an existing tracker.', chalk.yellow('track subtract <name> <amount>'), chalk.yellow('track - <name> <amount>'), '  Subtract any amount of time from an existing tracker.', '', chalk.yellow('track status <name>'), chalk.yellow('track s <name>'), '  Show the status of a tracker.', chalk.yellow('track status'), chalk.yellow('track s'), '  Show the status of all active trackers.', '', chalk.yellow('Options:'), '  -s, --silent		No output', '  -p, --porcelain	Machine-readable output.'].join('\n') + '\n';
-
-start = function(name, options) {
-  if (options == null) {
-    options = {};
-  }
-  if (!name) {
-    process.stderr.write('Missing `name` argument.');
-    return process.exit(1);
-  }
-  return track.start(name).then(function(ctx) {
-    if (options.silent) {
-      return;
-    }
-    return process.stdout.write([figures.started, chalk.underline(name), chalk.gray, ctx.isNew ? 'started' : ctx.wasRunning ? 'already running' : 'resumed'].join(' ') + '\n');
-  });
-};
-
-stop = function(name, options) {
-  if (options == null) {
-    options = {};
-  }
-  if (!name) {
-    process.stderr.write('Missing `name` argument.');
-    return process.exit(1);
-  }
-  return track.stop(name)["catch"](showError).then(function(ctx) {
-    if (options.silent) {
-      return;
-    }
-    return process.stdout.write([figures.stopped, chalk.underline(name), chalk.gray('stopped')].join(' ') + '\n');
-  });
-};
-
-add = function(name, amount, options) {
-  if (options == null) {
-    options = {};
-  }
-  if (!name) {
-    process.stderr.write('Missing `name` argument.');
-    return process.exit(1);
-  }
-  if (!amount) {
-    process.stderr.write('Missing `amount` argument.');
-    return process.exit(1);
-  }
-  amount = ms(amount);
-  return track.add(name, amount)["catch"](showError).then(function(ctx) {
-    if (options.silent) {
-      return;
-    }
-    return process.stdout.write([chalk.gray('added'), ms(amount), chalk.gray('to'), chalk.underline(name)].join(' ') + '\n');
-  });
-};
-
-subtract = function(name, amount, options) {
-  if (options == null) {
-    options = {};
-  }
-  if (!name) {
-    process.stderr.write('Missing `name` argument.');
-    return process.exit(1);
-  }
-  if (!amount) {
-    process.stderr.write('Missing `amount` argument.');
-    return process.exit(1);
-  }
-  amount = ms(amount);
-  return track.subtract(name, amount)["catch"](showError).then(function(ctx) {
-    if (options.silent) {
-      return;
-    }
-    return process.stdout.write([chalk.gray('subtracted'), ms(amount), chalk.gray('from'), chalk.underline(name)].join(' ') + '\n');
-  });
-};
-
-singleStatus = function(task) {
-  var elapsed, output;
-  elapsed = tracker.started ? Date.now() - tracker.started : 0;
-  output = [lPad(chalk.underline(task.name), 25), rPad(chalk.cyan(ms(tracker.value + elapsed)), 15)];
-  if (tracker.started) {
-    output.push(figures.started, ms(elapsed));
-  }
-  return output.join('');
-};
-
-status = function(name, options) {
-  if (options == null) {
-    options = {};
-  }
-  return track.read(name)["catch"](showError).then(function(tasks) {
-    var count, task;
-    if (options.silent) {
-      return;
-    }
-    count = 0;
-    for (name in tasks) {
-      task = tasks[name];
-      count++;
-      process.stdout.write(singleStatus(task) + '\n');
-    }
-    if (count === 0) {
-      return process.stdout.write(chalk.gray('no trackers\n'));
-    }
-  })["catch"](showError);
-};
-
-argv = yargs.argv;
-
-options = {
-  silent: argv.silent || argv.s || false,
-  porcelain: argv.porcelain || argv.p || false
-};
-
-switch (argv._[0]) {
-  case 'start':
-  case '-':
-    start(argv._[1], options);
-    break;
-  case 'stop':
-  case '.':
-    start(argv._[1], options);
-    break;
-  case 'add':
-  case '+':
-    add(argv._[1], argv._[2], options);
-    break;
-  case 'subtract':
-  case '-':
-    subtract(argv._[1], argv._[2], options);
-    break;
-  case 'status':
-  case 's':
-    status(argv._[1], options);
-    break;
-  default:
-    if (argv.help || argv.h) {
-      process.stdout.write(help);
-    } else {
-      process.stderr.write('invalid command\n');
-    }
+const figures = {
+	started:  chalk.green('\u25b6'),
+	stopped:  chalk.red('\u25a0'),
+	error:    chalk.red('!')
 }
+
+const showError = (err) => process.stderr.write([
+	figures.error, err.message
+].join(' ') + '\n')
+
+
+
+const start = Promise.coroutine(function* (name, options) {
+	debugger
+	if (!options) options = {}
+	if (!name) {
+		process.stderr.write('Missing `name` argument.')
+		return process.exit(1)
+	}
+
+	let result = yield track.start(name)
+	if (result instanceof Error) return showError(result)
+	if (options.silent) return
+
+	process.stdout.write([
+		figures.started,
+		chalk.underline(name),
+		chalk.gray(result.isNew ? 'started' :
+			(result.wasRunning ? 'already running' : 'resumed')
+		)
+	].join(' ') + '\n')
+})
+
+
+
+const stop = Promise.coroutine(function* (name, options) {
+	debugger
+	if (!options) options = {}
+	if (!name) {
+		process.stderr.write('Missing `name` argument.')
+		return process.exit(1)
+	}
+
+	return track.stop(name)
+	if (result instanceof Error) return showError(result)
+	if (options.silent) return
+
+	process.stdout.write([
+		figures.stopped,
+		chalk.underline(name),
+		chalk.gray('stopped')
+	].join(' ') + '\n')
+})
+
+
+
+const add = Promise.coroutine(function* (name, amount, options) {
+	debugger
+	if (!options) options = {}
+	if (!name) {
+		process.stderr.write('Missing `name` argument.')
+		return process.exit(1)
+	}
+	if (!amount) {
+		process.stderr.write('Missing `amount` argument.')
+		return process.exit(1)
+	}
+	amount = ms(amount)
+
+	let result = yield track.add(name, amount)
+	if (result instanceof Error) return showError(result)
+	if (options.silent) return
+
+	process.stdout.write([
+		chalk.gray('added'),
+		ms(amount),
+		chalk.gray('to'),
+		chalk.underline(name)
+	].join(' ') + '\n')
+})
+
+
+
+const subtract = Promise.coroutine(function* (name, amount, options) {
+	debugger
+	if (!options) options = {}
+	if (!name) {
+		process.stderr.write('Missing `name` argument.')
+		return process.exit(1)
+	}
+	if (!amount) {
+		process.stderr.write('Missing `amount` argument.')
+		return process.exit(1)
+	}
+
+	amount = ms(amount)
+	track.subtract(name, amount)
+	if (result instanceof Error) return showError(result)
+	if (options.silent) return
+
+	process.stdout.write([
+		chalk.gray('subtracted'),
+		ms(amount),
+		chalk.gray('from'),
+		chalk.underline(name)
+	].join(' ') + '\n')
+})
+
+
+
+const statusOfTask = (tracker) => {
+	let elapsed = tracker.started ? Date.now() - tracker.started : 0
+	let output = [
+		lPad(chalk.underline(tracker.name), 25),
+		rPad(chalk.cyan(ms(tracker.value + elapsed)), 15)
+	]
+	if (tracker.started) output.push(figures.started, ms(elapsed))
+	return output.join(' ')
+}
+
+const status = Promise.coroutine(function* (name, options) {
+	debugger
+	if (!options) options = {}
+	let trackers = yield track.read(name)
+	if (trackers instanceof Error) return showError(trackers)
+	if (options.silent) return
+
+	if (Object.keys(trackers).length === 0) {
+		process.stdout.write(chalk.gray('no trackers\n'))
+		return process.exit(1)
+	}
+
+	for (name in trackers)
+		process.stdout.write(statusOfTask(trackers[name]) + '\n')
+})
+
+
+
+const help = [
+	chalk.yellow('track start <name>'),
+	chalk.yellow('track - <name>'),
+	'  Start a new or resume an existing tracker. `name` must be a valid JSON key.',
+	chalk.yellow('track stop <name>'),
+	chalk.yellow('track . <name>'),
+	'  Stop an existing tracker.',
+	'',
+	chalk.yellow('track add <name> <amount>'),
+	chalk.yellow('track + <name> <amount>'),
+	'  Add any amount of time to an existing tracker.',
+	chalk.yellow('track subtract <name> <amount>'),
+	chalk.yellow('track - <name> <amount>'),
+	'  Subtract any amount of time from an existing tracker.',
+	'',
+	chalk.yellow('track status <name>'),
+	chalk.yellow('track s <name>'),
+	'  Show the status of a tracker.',
+	chalk.yellow('track status'),
+	chalk.yellow('track s'),
+	'  Show the status of all active trackers.',
+	'',
+	chalk.yellow('Options:'),
+	'  -s, --silent   No output',
+	'  -p, --porcelain  Machine-readable output.'
+].join('\n') + '\n'
+
+const argv = yargs.argv
+const options = {
+	silent:    argv.silent || argv.s || false,
+	porcelain: argv.porcelain || argv.p || false
+}
+
+debugger
+switch (argv._[0]) {
+	case 'start':
+	case '-':
+		start(argv._[1], options)
+		break
+	case 'stop':
+	case '.':
+		stop(argv._[1], options)
+		break
+	case 'add':
+	case '+':
+		add(argv._[1], argv._[2], options)
+		break
+	case 'subtract':
+	case '-':
+		subtract(argv._[1], argv._[2], options)
+		break
+	case 'status':
+	case 's':
+		status(argv._[1], options)
+		break
+	default:
+		if (argv.help || argv.h) process.stdout.write(help)
+		else process.stderr.write('invalid command\n')
+		break
+}
+// end
+debugger
--- src/index.js
+++ src/index.js
@@ -1,110 +1,87 @@
-var Promise, fs, home, path;
+'use strict'
 
-path = require('path');
+const path =    require('path')
+const home =    require('os-homedir')()
+const Promise = require('bluebird')
+const fs =      require('fs-promise')
 
-home = require('os-homedir')();
 
-fs = require('fs-promise');
 
-Promise = require('bluebird');
 
-module.exports = {
-  file: path.join(home, 'time-tracking', 'trackers.json'),
-  read: function(tracker) {
-    var dir, file;
-    file = this.file;
-    dir = path.dirname(file);
-    return new Promise(function(resolve, reject) {
-      return fs.stat(dir)["catch"](function() {
-        return fs.mkdir(dir);
-      })["catch"](reject).then(function(stats) {
-        if (!stats.isDirectory()) {
-          return reject(new Error(dir + " is not a directory"));
-        } else {
-          return fs.readFile(file);
-        }
-      })["catch"](reject).then(function(trackers) {
-        trackers = JSON.parse(trackers);
-        if (tracker) {
-          if (!trackers[tracker]) {
-            return reject(new Error(tracker + " doesn't exist."));
-          } else {
-            return resolve(trackers[tracker]);
-          }
-        } else {
-          return resolve(trackers);
-        }
-      });
-    });
-  },
-  _write: function(trackers) {
-    var dir, file;
-    file = this.file;
-    dir = path.dirname(file);
-    return new Promise(function(resolve, reject) {
-      return fs.writeFile(file, JSON.stringify(trackers))["catch"](reject).then(function() {
-        return resolve(true);
-      });
-    });
-  },
-  start: function(tracker) {
-    var now, result;
-    now = Date.now();
-    result = {};
-    return this.read().then(function(trackers) {
-      if (trackers[tracker]) {
-        result.isNew = false;
-        if (trackers[tracker].started) {
-          result.wasRunning = true;
-        } else {
-          result.wasRunning = false;
-          trackers[tracker].started = now;
-        }
-      } else {
-        result.isNew = true;
-        result.wasRunning = false;
-        trackers[tracker] = {
-          name: tracker,
-          started: now,
-          value: 0
-        };
-      }
-      return trackers;
-    }).then(this._write).then(function() {
-      return result;
-    });
-  },
-  stop: function(tracker) {
-    var now, result;
-    now = Date.now();
-    result = {};
-    return this.read().then(function(trackers) {
-      if (!trackers[tracker]) {
-        return new Error(tracker + " doesn't exist.");
-      }
-      if (trackers[tracker].started) {
-        result.wasRunning = true;
-        trackers[tracker].value += now - trackers[tracker].started;
-        trackers[tracker].started = false;
-      } else {
-        result.wasRunning = false;
-      }
-      return trackers;
-    }).then(this._write).then(function() {
-      return result;
-    });
-  },
-  add: function(tracker, amount) {
-    var now;
-    now = Date.now();
-    return this.read().then(function(trackers) {
-      if (!trackers[tracker]) {
-        return new Error(tracker + " doesn't exist.");
-      }
-      trackers[tracker].value += amount;
-      return trackers;
-    }).then(this._write).then(function() {
-      return null;
-    });
-  }
-};
+
+module.exports = (file) => {
+	if (!file) file = path.join(home, 'time-tracking/trackers.json')
+	let dir = path.dirname(file)
+	let self = {
+
+
+
+		read: Promise.coroutine(function* (name) {
+			let stats = yield fs.stat(dir)
+			if (!stats || !stats.isDirectory()) throw new Error(`${dir} is not a directory`)
+			let trackers = JSON.parse(yield fs.readFile(file))
+			if (!name) return trackers
+			if (trackers[name]) return trackers[name]
+			throw new Error(`${name} doesn't exist.`)
+		}),
+
+
+
+		_write: ((trackers) => fs.writeFile(file, JSON.stringify(trackers))),
+
+
+
+		start: Promise.coroutine(function* (name) {
+			let now = Date.now()
+			let result = {}
+			let trackers = yield self.read()
+			if (!trackers[name]) {
+				result.isNew = true
+				result.wasRunning = false
+				trackers[name] = {name: name, started: now, value: 0}
+			} else {
+				result.isNew = false
+				if (trackers[name].started) result.wasRunning = true
+				else {
+					result.wasRunning = false
+					trackers[name].started = now
+				}
+			}
+			yield self._write(trackers)
+			return result
+		}),
+
+
+
+		stop: Promise.coroutine(function* (name) {
+			let now = Date.now()
+			let result = {}
+			let trackers = yield self.read()
+			if (!trackers[name]) throw new Error(`${name} doesn't exist.`)
+			if(trackers[name].started) {
+				result.wasRunning = true
+				trackers[name].value += now - trackers[name].started
+				trackers[name].started = false
+			} else result.wasRunning = false
+			yield self._write(trackers)
+			return result
+		}),
+
+
+
+		add: Promise.coroutine(function* (name, amount) {
+			debugger
+			let now = Date.now()
+			let trackers = yield self.read()
+			if (!trackers[name]) throw new Error(`${name} doesn't exist.`)
+			trackers[name].value += amount
+			console.log('trackers', trackers)
+			yield self._write(trackers)
+			return null
+		})
+
+
+
+	}
+	return self
+}
